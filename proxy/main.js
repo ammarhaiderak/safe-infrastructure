a48949d5f8dddfcde9bbb3a864e172dc7f6a3fc7
Ammar Haider <ammarhaiderak@github.com>
2023-09-07 14:46:25 +0000

    update infra

--- proxy/main.js
+++ proxy/main.js
@@ -1,6 +1,12 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
+const cors = require('cors');
 
+const corsOptions = {
+  origin: '*', // Adjust this to allow requests from specific origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  optionsSuccessStatus: 204,
+};
 
 const targetUrl = 'http://localhost:8000';  // The local service URL
 const proxy = httpProxy.createProxyServer({});
@@ -9,12 +15,27 @@ const server = http.createServer((req, res) => {
   // Log the incoming request if needed
   console.log(`Proxying request: ${req.method} ${req.url}`);
   
-//   req.headers['origin'] = 'https://api.wagmilabz.com';
-//   req.headers['referer'] = 'https://api.wagmilabz.com/';
+  req.headers['origin'] = 'http://localhost:8000';
+  req.headers['referer'] = 'http://localhost:8000/';
   // Proxy the request to the local service
   proxy.web(req, res, { target: targetUrl });
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  // Set the 'Access-Control-Allow-Origin' header in the response
+  res.setHeader('Access-Control-Allow-Origin', corsOptions.origin);
+  res.setHeader('Access-Control-Allow-Methods', corsOptions.methods);
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Handle preflight request
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
 });
+
+
+// Use the cors middleware
+// server.use(cors(corsOptions));
 
 const PORT = 8383;  // Port for your proxy server to listen on
 server.listen(PORT, () => {
